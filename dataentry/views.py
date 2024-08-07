c39c4b8878942575c994cac343a52398f93eff34
jason <jyou8304@gmail.com>
2024-08-07 11:02:37 -0400

    import data

--- dataentry/views.py
+++ dataentry/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .utils import get_all_custom_models
+from uploads.models import Upload
+from django.conf import settings
+from django.core.management import call_command
 
 # Create your views here.
 def import_data(request):
-    return render(request, 'dataentry/importdata.html')
+    if request.method == "POST":
+        file_path = request.FILES.get('file_path')
+        model_name = request.POST.get('model_name')
+
+        upload = Upload.objects.create(file=file_path, model_name=model_name)
+
+        relative_path = str(upload.file.url)
+        base_url = str(settings.BASE_DIR)
+
+        file_path = base_url + relative_path
+        try:
+            call_command('importdata',file_path, model_name)
+        except Exception as e:
+            raise e
+
+        return redirect('import_data')
+    else:
+        custom_models= get_all_custom_models()
+        context = {
+            'custom_models': custom_models
+        }
+    
+    return render(request, 'dataentry/importdata.html', context)
